49b16bfe016e4279066e4657e1a8690c764b3948
CodeRedstone <blafblabla@hotmail.co.uk>
2011-12-06 13:01:24 +0000

    Might work, dunno

--- RadPerkss/src/me/draosrt/radperks/RadPerks.java
+++ RadPerkss/src/me/draosrt/radperks/RadPerks.java
@@ -14,7 +14,7 @@ public class RadPerks extends JavaPlugin {
 		this.log = new RadPerksLogHandler (this);
 		PluginManager pm = this.getServer().getPluginManager();
 		pm.registerEvent(Event.Type.PLAYER_INTERACT, this.playerListener, Event.Priority.Normal, this);
-		
+		this.load();
 		this.log.info("Enabled");
 		
 	}
@@ -24,6 +24,32 @@ public class RadPerks extends JavaPlugin {
 		this.log.info("Disabled");
 		
 		
+	}
+	private void load(){
+		int i;
+		boolean bla = true;
+		playerListener.myHashMap.clear();  // clear the hashmap, should be clear anyway. you need to make it public in playerListener aswell.
+		BufferedReader read = new BufferedReader(new FileReader(new File("myfile.txt")); // same file...	
+		try {
+			do{
+				String s; // new string
+				if((s =  read.readLine()) == null) { // if the next line doesn't exist <-- fail spelling
+					break; // stop
+				}
+			
+				
+				String[] rads = s.split(":"); //split at :, same syntax as before
+				playerListener.myHashMap.put(rads[0], Integer.parseInt(rads[1 // put it in the hashmap
+				
+			
+			}while(true);	
+			
+		}catch(IOException e) {
+			e.printStackTrace();
+		}catch(Exception e) {
+			
+		}
+			
 	}
 
 	
